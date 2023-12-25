1885a3292c27c37b4db40d668edefc9a4b35900a
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2023-12-25 12:56:55 +0000

    save the note to localstorage and add check for empty inputs

--- note.js
+++ note.js
@@ -3,6 +3,8 @@ const inputNote = document.querySelector('.inputs');
 const submitBtn = document.querySelector('.submitbtn');
 const noteArea = document.querySelector('.js-note-area');
 
+noteArea.innerHTML = localStorage.getItem('note');
+
 addNoteBtn.addEventListener('click',()=>{
   inputNote.classList.add('show');
   addNoteBtn.classList.add('notShow');
@@ -23,24 +25,28 @@ submitBtn.addEventListener('click',()=>{
 });
 
 function getNote(titleInput,noteInput){
-  let noteHtml = '';
 
   const title = titleInput.value
   const note = noteInput.value
-  noteHtml = `
-  <div class="note">
-  <h2 class="title">${title}</h2>
-  <p class="note-body">${note}</p>
-  <a href="#">Read More</a>
-</div>
-  `
-  
-  titleInput.value = '';
-  noteInput.value = '';
-  
-  const mainContainer = document.querySelector('.js-note-area');
+  if (title.trim() !== '' && note.trim() !== '') {
+    const noteHtml = `
+      <div class="note">
+        <h2 class="title">${title}</h2>
+        <p class="note-body">${note}</p>
+        <a href="#">Read More</a>
+      </div>
+    `;
 
-  mainContainer.innerHTML += noteHtml;
+    titleInput.value = '';
+    noteInput.value = '';
 
+    const mainContainer = document.querySelector('.js-note-area');
+    mainContainer.innerHTML += noteHtml;
+
+    SaveToStorage(mainContainer.innerHTML);
+  }
 }
 
+function SaveToStorage(notesHtml){
+  localStorage.setItem('note',notesHtml)
+}
