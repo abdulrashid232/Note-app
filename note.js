75ed11d5252a1c1b74cea6e3d0a54887fb708732
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2023-12-25 14:50:04 +0000

    save the new data after the delete btn

--- note.js
+++ note.js
@@ -1,10 +1,12 @@
+
 const addNoteBtn = document.querySelector('.js-add-note');
 const inputNote = document.querySelector('.inputs');
 const submitBtn = document.querySelector('.submitbtn');
 const noteArea = document.querySelector('.js-note-area');
 
-noteArea.innerHTML = localStorage.getItem('note');
 
+noteArea.innerHTML = localStorage.getItem('note');
+DeleteNote();
 addNoteBtn.addEventListener('click',()=>{
   inputNote.classList.add('show');
   addNoteBtn.classList.add('notShow');
@@ -15,29 +17,28 @@ submitBtn.addEventListener('click',()=>{
   const titleInput = document.querySelector('.inputForTitle');
   const noteInput = document.querySelector('.inputForNote');
 
-
   getNote(titleInput, noteInput);
   inputNote.classList.toggle('show');
   addNoteBtn.classList.toggle('notShow');
   noteArea.classList.toggle('notShow');
-  
-  
 });
 
-function getNote(titleInput,noteInput){
+function getNote(titleInput,noteInput) {
 
-  const title = titleInput.value
-  const note = noteInput.value
+
+  const title = titleInput.value;
+  const note = noteInput.value;
+
   if (title.trim() !== '' && note.trim() !== '') {
     const noteHtml = `
-      <div class="note js-note-${title}" data-note-id="${title}">
-       <div class="head">
-        <h2 class="title">${title}</h2>
-        <div>
-          <button class="deleteBtn" data-note-id="${title}">X</button>
-          <button class="editBtn">edit</button>
+      <div class="note js-note" data-note-id="${title}">
+        <div class="head">
+          <h2 class="title">${title}</h2>
+          <div>
+            <button class="deleteBtn" data-note-id="${title}">X</button>
+            <button class="editBtn">edit</button>
+          </div>
         </div>
-       </div>
         <p class="note-body">${note}</p>
         <a href="#">Read More</a>
       </div>
@@ -50,30 +51,21 @@ function getNote(titleInput,noteInput){
     mainContainer.innerHTML += noteHtml;
 
     SaveToStorage(mainContainer.innerHTML);
-    DeleteNote(mainContainer)
-  
+    DeleteNote();
   }
-  
-  
 }
 
-
-function SaveToStorage(notesHtml){
-  localStorage.setItem('note',notesHtml)
+function SaveToStorage(notesHtml) {
+  localStorage.setItem('note', notesHtml);
 }
 
-
-function DeleteNote(mainContainer){
-document.querySelectorAll('.deleteBtn')
-  .forEach((btn)=>{
-    
-    btn.addEventListener('click',()=>{
+function DeleteNote() {
+  document.querySelectorAll('.deleteBtn').forEach((btn) => {
+    btn.addEventListener('click', () => {
       const title = btn.dataset.noteId;
-      const noteContainer =document.querySelector(`.js-note-${title}`);
-      mainContainer.remove(noteContainer);
-      SaveToStorage(mainContainer.innerHTML)
-      console.log(noteContainer)
+      const noteContainer = document.querySelector(`[data-note-id="${title}"]`);
+      noteContainer.remove();
+      SaveToStorage(document.querySelector('.js-note-area').innerHTML);
     });
   });
- 
 }
