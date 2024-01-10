e491dd040e84fe4d3ea044a987ad0e35e00d544e
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-01-10 15:05:57 +0000

    made the edit button interactive

--- note.js
+++ note.js
@@ -2,15 +2,18 @@
 const addNoteBtn = document.querySelector('.js-add-note');
 const inputNote = document.querySelector('.inputs');
 const submitBtn = document.querySelector('.submitbtn');
+const saveEditBtn = document.querySelector('.saveEdit');
 const noteArea = document.querySelector('.js-note-area');
 
 
 noteArea.innerHTML = localStorage.getItem('note');
 DeleteNote();
+EditNote();
 addNoteBtn.addEventListener('click',()=>{
   inputNote.classList.add('show');
   addNoteBtn.classList.add('notShow');
   noteArea.classList.add('notShow');
+  saveEditBtn.classList.add('notShow');
 });
 
 submitBtn.addEventListener('click',()=>{
@@ -31,15 +34,15 @@ function getNote(titleInput,noteInput) {
 
   if (title !== '' && note !== '') {
     const noteHtml = `
-      <div class="note js-note" data-note-id="${title}">
+      <div class="note js-note-${title}" data-note-id="${title}">
         <div class="head">
-          <h2 class="title" data-note-title-id="${title}">${title}</h2>
+          <h2 class="title js-title-${title}">${title}</h2>
           <div>
             <button class="deleteBtn" data-note-id="${title}">X</button>
-            <button class="editBtn">edit</button>
+            <button class="editBtn" data-note-id="${title}">edit</button>
           </div>
         </div>
-        <p class="note-body" data-note-id="${title}">${note}</p>
+        <p class="note-body js-noteBody-${title}">${note}</p>
         <a href="#">Read More</a>
       </div>
     `;
@@ -64,7 +67,7 @@ function DeleteNote() {
   document.querySelectorAll('.deleteBtn').forEach((btn) => {
     btn.addEventListener('click', () => {
       const title = btn.dataset.noteId;
-      const noteContainer = document.querySelector(`[data-note-id="${title}"]`);
+      const noteContainer = document.querySelector(`.js-note-${title}`);
       noteContainer.remove();
       SaveToStorage(document.querySelector('.js-note-area').innerHTML);
     });
@@ -75,17 +78,29 @@ function EditNote(){
   document.querySelectorAll('.editBtn').forEach((btn) => {
     btn.addEventListener('click', () => {
       const title = btn.dataset.noteId;
-      const noteTitle = document.querySelector(`[data-note-title-id="${title}"]`);
+      const noteTitle = document.querySelector(`.js-title-${title}`);
+      const noteBody = document.querySelector(`.js-noteBody-${title}`);
+     
+
+
+      console.log(noteTitle.textContent);
+      console.log(noteBody.textContent);
       const titleInput = document.querySelector('.inputForTitle');
       const noteInput = document.querySelector('.inputForNote');
-      const newTitle = titleInput.value.trim();
-      const note = noteInput.value.trim();
-      console.log(newTitle);
-      // console.log(noteTitle.textContent);
+      titleInput.value = noteTitle.textContent;
+      noteInput.value = noteBody.textContent;
+
       inputNote.classList.add('show');
       addNoteBtn.classList.add('notShow');
       noteArea.classList.add('notShow');
-      // SaveToStorage(document.querySelector('.js-note-area').innerHTML);
+      submitBtn.classList.add('notShow');
+      saveEditBtn.classList.add('show');
+      saveEditBtn.addEventListener('click', ()=>{
+        getNote(titleInput, noteInput);
+      })
+
+      
     });
   });
 }
+
