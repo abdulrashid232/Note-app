7926d6f97e5769176f4fb5b9bad1c82c4364b969
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-02-06 13:18:17 +0000

    convert it to pwa

--- note.js
+++ note.js
@@ -10,6 +10,16 @@ noteArea.innerHTML = localStorage.getItem('note');
 DeleteNote();
 EditNote();
 ReadMore();
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+          console.error('Service Worker registration failed:', error);
+      });
+}
 addNoteBtn.addEventListener('click', () => {
   inputNote.classList.add('show');
   addNoteBtn.classList.add('notShow');
